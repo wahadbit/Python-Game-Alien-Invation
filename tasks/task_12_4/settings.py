1f4ebddc3574b8952806271960877d6309636acc
Timur Sarsembai <118261557+wahadbit@users.noreply.github.com>
2023-01-12 19:00:24 +0600

    solved task 12.4

---
+++ tasks/task_12_4/settings.py
@@ -0,0 +1,8 @@
+class Settings():
+
+    def __init__(self):
+        ''''''
+        self.bg_color = (135, 206, 235)
+        self.screen_width = 900
+        self.screen_height = 600
+        self.ship_speed = 1.5
---
+++ tasks/task_12_4/task_12_4.py
@@ -0,0 +1,71 @@
+import sys
+
+import pygame
+
+from ship import Ship
+from settings import Settings
+
+
+class RocketMover():
+    '''12.4. Ракета: '''
+
+    def __init__(self):
+
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+
+        pygame.display.set_caption('Task 12.2')
+
+        self.ship = Ship(self)
+
+    def start(self):
+        """Запуск основного цикла игры."""
+        while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        ''''''
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False     
+
+    def _update_screen(self):
+        # При каждом проходе цикла перерисовывается экран.
+        self.screen.fill(self.settings.bg_color)
+        self.ship.bletme()
+
+        # Отображение последнего прорисованного экрана.
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = RocketMover()
+    ai.start()
